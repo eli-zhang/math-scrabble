d5a35594832a180b676ac480f218053e4f9ccf4a
eli-zhang <zeliboid8@gmail.com>
2023-09-17 14:54:53 -0400

    Support adding/removing, disable hand, calculate scoer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { tileCount } from './tileInfo'
+import { tileCounts, tileScores } from './tileInfo'
 import { configureTiles } from './generateTiles';
 
 function App() {
@@ -30,22 +30,65 @@ function App() {
     ["", "3E", "", "", "", "3S", "", "", "", "2S", "", "", "", "3S", "", "", "", "3E", ""],
   ];
   
-  const allTiles = Object.entries(tileCount).flatMap(([key, val]) => {
+  const allTiles = Object.entries(tileCounts).flatMap(([key, val]) => {
     return Array(val).fill(key);
   });
 
   const [currentHand, setCurrentHand] = React.useState<string[]>(["="].concat(allTiles.sort(() => Math.random() - 0.5).slice(0, 9)));
+  const [usedTilesInHandIdx, setUsedTilesInHandIdx] = React.useState<number[]>([]);
 
   const specialTiles = new Set(["3E", "3S", "2S", "2E"]);
   const initialPlacedTiles: string[][] = Array.from({length: 19}, () => Array(19).fill(""));
+  const initialTilePositions: boolean[][] = Array.from({length: 19}, () => Array(19).fill(false));
 
   const [selectedTile, setSelectedTile] = React.useState<string | null>(null);
   const [placedTiles, setPlacedTiles] = React.useState<string[][]>(initialPlacedTiles);
   const [boardToRender, setBoardToRender] = React.useState<string[][]>(boardLayout);
+  const [pendingTilePositions, setPendingTilePositions] = React.useState<boolean[][]>(initialTilePositions);
+  const [totalScore, setTotalScore] = React.useState<number>(0);
 
-  const handleTileClick = (letter: string) => {
+  function deepCopy(arr: any[]) {
+    let copy: any[] = [];
+    arr.forEach(elem => {
+      if (Array.isArray(elem)) {
+        copy.push(deepCopy(elem));
+      } else {
+        copy.push(elem);
+      }
+    });
+    return copy;
+  }
+
+  const handleTileInHandClick = (letter: string, index: number) => {
+    if (!usedTilesInHandIdx.includes(index)) {
+      setSelectedTile(letter);
+      setUsedTilesInHandIdx([...usedTilesInHandIdx, index]);
+    }
     setSelectedTile(letter);
   };
+
+  const handleTileOnBoardClick = (rowIndex: number, colIndex: number) => {
+    if (tilePendingOnBoard(rowIndex, colIndex)) {
+      const tileValue = placedTiles[rowIndex][colIndex]
+      const newPlacedTiles = [...placedTiles];
+      newPlacedTiles[rowIndex][colIndex] = "";
+      setPlacedTiles(newPlacedTiles);
+      
+      const newPendingTilePositions = [...pendingTilePositions];
+      newPendingTilePositions[rowIndex][colIndex] = false;
+      setPendingTilePositions(newPendingTilePositions);
+
+      usedTilesInHandIdx.forEach((idx) => {
+        if (currentHand[idx] === tileValue) {
+          setUsedTilesInHandIdx(usedTilesInHandIdx.filter((index) => index !== idx));
+        }
+      });
+    }
+  }
+
+  const tilePendingOnBoard = (rowIndex: number, colIndex: number) => {
+    return pendingTilePositions[rowIndex][colIndex];
+  }
 
   const handleCellClick = (rowIndex: number, colIndex: number) => {
     if (selectedTile) {
@@ -53,11 +96,43 @@ function App() {
       newPlacedTiles[rowIndex][colIndex] = selectedTile;
       setPlacedTiles(newPlacedTiles);
       setSelectedTile(null);
+
+      const newPendingTilePositions = [...pendingTilePositions];
+      newPendingTilePositions[rowIndex][colIndex] = true;
+      setPendingTilePositions(newPendingTilePositions);
     }
   };
+
+  const getCurrentPlayScore = () => {
+    let sum = 0;
+    let multiplier = 1; // initialize multiplier to 1
+    pendingTilePositions.forEach((row, rowIndex) => {
+      row.forEach((isPending, colIndex) => {
+        if (isPending) {
+          const placedTile = placedTiles[rowIndex][colIndex];
+          const tileScore = tileScores[placedTile];
+          const boardSpace = boardLayout[rowIndex][colIndex];
+          console.log(boardLayout)
+          if (boardSpace === "3S") {
+            sum += tileScore * 3; // multiply tile score by 3 if "3S" is present
+          } else if (boardSpace === "2S") {
+            sum += tileScore * 2; // multiply tile score by 2 if "2S" is present
+          } else {
+            sum += tileScore; // add tile score if no multiplier is present
+          }
+          if (boardSpace === "3E") {
+            multiplier *= 3; // set multiplier to 3 if "3E" is present
+          } else if (boardSpace === "2E") {
+            multiplier *= 2; // set multiplier to 2 if "2E" is present
+          }
+        }
+      });
+    });
+    return sum * multiplier;
+  }
   
   React.useEffect(() => {
-    const newBoardToRender = [...boardToRender];
+    const newBoardToRender = deepCopy(boardLayout);
     placedTiles.forEach((row, rowIndex) => {
       row.forEach((cellContent, colIndex) => {
         if (cellContent !== "") {
@@ -65,7 +140,9 @@ function App() {
         }
       });
     });
+
     setBoardToRender(newBoardToRender);
+    setTotalScore(getCurrentPlayScore());
   }, [placedTiles])
 
   return (
@@ -75,21 +152,12 @@ function App() {
           {boardToRender.map((row, rowIndex) =>{
             return row.map((cellContent, colIndex) => {
 
-              let cellColor = "rgb(108, 108, 213)"; // default color
-
-              if (cellContent === "3E") { // if cell content is "3E", color it red
-                cellColor = "rgb(173, 44, 158)";
-              } else if (cellContent === "3S") { // if cell content is "3S", color it green
-                cellColor = "rgb(7, 104, 86)";
-              } else if (cellContent === "2E") { // if cell content is "3S", color it green
-                cellColor = "rgb(80, 22, 121)";
-              } else if (cellContent === "2S") { // if cell content is "3S", color it green
-                cellColor = "rgb(44, 60, 178)";
-              } else if (!specialTiles.has(cellContent) && cellContent != "") { // if cell content is not a special tile
+              if (!specialTiles.has(cellContent) && cellContent != "") {
+                // Render a tile on the board instead.
                 return (
                   <div 
                     key={`${rowIndex}-${colIndex}`}
-                    // onClick={() => handleTileClick(cellContent)}
+                    onClick={() => handleTileOnBoardClick(rowIndex, colIndex)}
                     className={`grid-tile ${cellContent.length > 1 ? 'small-font' : ''}`}
                     data-letter={cellContent}
                   />
@@ -99,8 +167,7 @@ function App() {
               return (
                 <div
                   key={`${rowIndex}-${colIndex}`}
-                  className="grid-cell"
-                  style={{ backgroundColor: cellColor }}
+                  className={`grid-cell space-${cellContent}`}
                   onClick={() => handleCellClick(rowIndex, colIndex)}
                 >
                   {cellContent}
@@ -111,29 +178,22 @@ function App() {
         </div>
       </div>
 
-      <div className="all">
+      <div>
+        Total score: {totalScore}
+      </div>
+
+      <div className="hand">
         {currentHand.map((tile, index) => {
           return (
 
             <div
               key={index}
-              className={`tile ${tile.length > 1 ? 'small-font' : ''}${tile === "=" ? 'equals' : ''}`}
+              className={`tile ${tile.length > 1 ? 'small-font' : ''}${tile === "=" ? 'equals' : ''} ${usedTilesInHandIdx.includes(index) ? 'used' : ''}`}
               data-letter={tile}
-              onClick={() => handleTileClick(tile)}
+              onClick={() => handleTileInHandClick(tile, index)}
             />
           )})}
       </div>
-
-      {/* <div className="all">
-      { Object.entries(tileCount).map(keyValArray => {
-        const key = keyValArray[0];
-        const val = keyValArray[1];
-        return Array(val).fill(<div onClick={() => { handleTileClick(key) }} className={`tile ${key.length > 1 ? 'small-font' : ''}`} data-letter={key}/>).map((ele, index) => React.cloneElement(
-          ele, { key: `${key}${index}` }
-        ))
-      })}
-      </div> */}
-
     </div>
   );
 }
