9332d23de4ec164354281b73e37a68a82522ba04
eli-zhang <zeliboid8@gmail.com>
2023-09-17 01:27:00 -0400

    Add basic board and tiles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { tileCount } from './tileInfo'
+import { configureTiles } from './generateTiles';
 
 function App() {
+  useEffect(() => {
+    configureTiles();
+  }, [])
+
+  const cellContents = [
+    ["", "3E", "", "", "", "3S", "", "", "", "2S", "", "", "", "3S", "", "", "", "3E", ""],
+    ["3E", "", "", "", "3S", "", "", "", "2S", "", "2S", "", "", "", "3S", "", "", "", "3E"],
+    ["", "", "", "2E", "", "", "", "2S", "", "", "", "2S", "", "", "", "2E", "", "", ""],
+    ["", "", "2E", "", "", "", "3S", "", "", "", "", "", "3S", "", "", "", "2E", "", ""],
+    ["", "3S", "", "", "", "2S", "", "", "", "3S", "", "", "", "2S", "", "", "", "3S", ""],
+    ["3S", "", "", "", "2S", "", "", "", "3S", "", "3S", "", "", "", "2S", "", "", "", "3S"],
+    ["", "", "", "3S", "", "", "", "2S", "", "", "", "2S", "", "", "", "3S", "", "", ""],
+    ["", "", "2S", "", "", "", "2S", "", "", "", "", "", "2S", "", "", "", "2S", "", ""],
+    ["", "2S", "", "", "", "3S", "", "", "2S", "", "2S", "", "", "3S", "", "", "", "2S", ""],
+    ["2S", "", "", "", "3S", "", "", "", "", "2E", "", "", "", "", "3S", "", "", "", "2S"],
+    ["", "2S", "", "", "", "3S", "", "", "2S", "", "2S", "", "", "3S", "", "", "", "2S", ""],
+    ["", "", "2S", "", "", "", "2S", "", "", "", "", "", "2S", "", "", "", "2S", "", ""],
+    ["", "", "", "3S", "", "", "", "2S", "", "", "", "2S", "", "", "", "3S", "", "", ""],
+    ["3S", "", "", "", "2S", "", "", "", "3S", "", "3S", "", "", "", "2S", "", "", "", "3S"],
+    ["", "3S", "", "", "", "2S", "", "", "", "3S", "", "", "", "2S", "", "", "", "3S", ""],
+    ["", "", "2E", "", "", "", "3S", "", "", "", "", "", "3S", "", "", "", "2E", "", ""],
+    ["", "", "", "2E", "", "", "", "2S", "", "", "", "2S", "", "", "", "2E", "", "", ""],
+    ["3E", "", "", "", "3S", "", "", "", "2S", "", "2S", "", "", "", "3S", "", "", "", "3E"],
+    ["", "3E", "", "", "", "3S", "", "", "", "2S", "", "", "", "3S", "", "", "", "3E", ""],
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div className="rack">
+        
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+        <div className="tile" data-letter="1"/>
+      </div> */}
+      <div className="grid">
+        {cellContents.map((row, rowIndex) =>{
+          return row.map((cellContent, colIndex) => {
+
+            let cellColor = "rgb(108, 108, 213)"; // default color
+
+            if (cellContent === "3E") { // if cell content is "3E", color it red
+              cellColor = "red";
+            } else if (cellContent === "3S") { // if cell content is "3S", color it green
+              cellColor = "green";
+            } else if (cellContent === "2E") { // if cell content is "3S", color it green
+              cellColor = "purple";
+            } else if (cellContent === "2S") { // if cell content is "3S", color it green
+              cellColor = "blue";
+            }
+
+            return (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                className="grid-cell"
+                style={{ backgroundColor: cellColor }}
+              >
+                {cellContent}
+              </div>
+            );
+          })
+})}
+      </div>
+      <div className="all">
+      { Object.entries(tileCount).map(keyValArray => {
+        const key = keyValArray[0];
+        const val = keyValArray[1];
+        return Array(val).fill(<div className={`tile ${key.length > 1 ? 'small-font' : ''}`} data-letter={key}/>).map((ele, index) => React.cloneElement(
+          ele, { key: `${key}${index}` }
+        ))
+      })}
+
+      </div>
+
     </div>
   );
 }
